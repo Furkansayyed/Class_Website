236905f48057d51ecee1994350e618de67cefe69
Furkan Sayyed <sayyedfurkan115@gmail.com>
2023-10-09 13:50:05 +0530

    updated

--- student_portal/utils.py
+++ student_portal/utils.py
@@ -3,4 +3,4 @@ from django.conf import settings
 
 def send_email_to_client(subject, message, recepient_list):
     from_email = settings.EMAIL_HOST_USER
-    send_mail(subject, message, from_email, recepient_list)
+    send_mail(subject, message, from_email, [recepient_list])
