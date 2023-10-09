418cda3fadbc178a750dc05b14737b311a7068f0
Furkan Sayyed <73273662+Furkansayyed@users.noreply.github.com>
2023-10-09 13:54:22 +0530

    Update settings.py

--- Class_Website/settings.py
+++ Class_Website/settings.py
@@ -174,10 +174,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-<<<<<<< HEAD
-EMAIL_HOST_USER = "hitechclasses6@gmail.com"
-EMAIL_HOST_PASSWORD = "nnkzeluhsnukjuvp"
-=======
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
->>>>>>> 7f372dc6262fe309de0ecd0935322693536dc70e
